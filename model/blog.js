346569886e3c4283c638d80ecbaea7dfc76ca64a
dike-code <dikeclintonobinna58@gmail.com>
2023-10-28 22:38:40 +0100

    mongoose-models-added

--- model/blog.js
+++ model/blog.js
@@ -1,4 +1,30 @@
+/** @format */
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-blogSchema = new Schema
+blogSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        banner: {
+            type: String,
+            required: true,
+        },
+        nugget: {
+            type: String,
+            required: true,
+        },
+        body: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const blog = mongoose.model('Blog', blogSchema);
+
+module.exports = blog;
