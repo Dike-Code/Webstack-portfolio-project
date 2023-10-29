b7f9a5580e9dfe93581456e35c1b0aeaaaf77006
dike-code <dikeclintonobinna58@gmail.com>
2023-10-29 20:48:37 +0100

    modified files

--- express.js
+++ express.js
@@ -6,15 +6,15 @@ const mongoose = require('mongoose');
 const app = express();
 const Blog = require('./models/blog');
 
-//mongoDB connect
-// const dbUrl =
-//     'mongodb+srv://dike:hellow@dike-code.zjfjv0l.mongodb.net/dike?retryWrites=true&w=majority';
-// mongoose
-//     .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then((result) => {
-//         return app.listen(3000) + console.log('db connected');
-//     })
-//     .catch((err) => console.log(err));
+// mongoDB connect
+const dbUrl =
+    'mongodb+srv://dike:hellow@dike-code.zjfjv0l.mongodb.net/dike?retryWrites=true&w=majority';
+mongoose
+    .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        return app.listen(3000) + console.log('db connected');
+    })
+    .catch((err) => console.log(err));
 
 // set enigine
 app.set('view engine', 'ejs');
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended: true}))
 // morgan
 app.use(morgan('dev'));
 
-app.listen(3000);
+// app.listen(3000);
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' });
